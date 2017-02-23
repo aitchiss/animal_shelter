2d44b477cb265bb76da392e00e77e2223d71a55d
Suzanne <suzanne.aitchison@gmail.com>
2017-02-23 15:31:35 +0000

    AdoptionStatus class added with save method, tested in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require('pry')
 require_relative('../models/animal_type.rb')
+require_relative('../models/adoption_status.rb')
 require_relative('sql_runner.rb')
 
 cat_type = AnimalType.new( { 'type' => 'cat' } )
@@ -9,6 +10,12 @@ rabbit_type = AnimalType.new( 'type' => 'rabbit' )
 cat_type.save
 dog_type.save
 rabbit_type.save
+
+ready_status = AdoptionStatus.new( { 'status' => 'Ready for adoption' } )
+not_ready_status = AdoptionStatus.new( { 'status' => 'Not ready for adoption' } )
+
+ready_status.save
+not_ready_status.save
 
 
 binding.pry
---
+++ models/adoption_status.rb
@@ -0,0 +1,16 @@
+class AdoptionStatus
+
+  attr_reader :id, :status
+
+  def initialize( options )
+    @id = options['id'].to_i if options['id']
+    @status = options['status']
+  end
+
+  def save()
+    sql ="INSERT INTO adoption_statuses (status) VALUES ('#{@status}') RETURNING *; "
+    status_hash = SqlRunner.run(sql).first
+    @id = status_hash['id'].to_i
+  end
+
+end
