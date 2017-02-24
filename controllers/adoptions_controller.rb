64ddeac19ddcb9c4092e0e12d6273a165c64fb54
Suzanne <suzanne.aitchison@gmail.com>
2017-02-24 14:12:12 +0000

    new adoption form created and tested

---
+++ controllers/adoptions_controller.rb
@@ -0,0 +1,26 @@
+require ('sinatra')
+require ('sinatra/contrib/all')
+require('date')
+require_relative ('../models/animal.rb')
+require_relative ('../models/adoption_status.rb')
+require_relative ('../models/adoption.rb')
+require_relative ('../models/animal_type.rb')
+require_relative ('../db/sql_runner.rb')
+
+get '/adoptions/new' do
+  @animals = Animal.all
+  @owners = Owner.all
+  erb(:"adoptions/new")
+end
+
+post '/adoptions' do
+  date = Date.today
+  owner_id = params['owner_id'].to_i
+  animal_id = params['animal_id'].to_i
+  adoption = Adoption.new({
+    'owner_id' => owner_id,
+    'animal_id' => animal_id,
+    'date' => date
+    })
+  adoption.save
+end
