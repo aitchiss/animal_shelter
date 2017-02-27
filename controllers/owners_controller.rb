9b45863d213bba01408ed57a2448642200258cf3
Suzanne <suzanne.aitchison@gmail.com>
2017-02-27 11:36:23 +0000

    ensured only animals ready for adoption passed to owner match view

--- controllers/owners_controller.rb
+++ controllers/owners_controller.rb
@@ -27,7 +27,7 @@ get '/owners/:id/match' do
   animals = Animal.all
   @matched_animals = []
   animals.each do |animal|
-    if @owner.match(animal.id) == true
+    if @owner.match(animal.id) == true && animal.get_adoption_status == "ready for adoption"
       @matched_animals << animal
     end
   end
