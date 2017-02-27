5105be6a333daab205836efd62b3d8fc65358f9d
Suzanne <suzanne.aitchison@gmail.com>
2017-02-27 12:14:25 +0000

    added number of animals needing adoption to the home page

--- app.rb
+++ app.rb
@@ -2,11 +2,13 @@ require('sinatra')
 require('sinatra/contrib/all')
 
 require_relative('./models/adoption.rb')
+require_relative('./models/animal.rb')
 require_relative('./controllers/animals_controller.rb')
 require_relative('./controllers/owners_controller.rb')
 require_relative('./controllers/adoptions_controller.rb')
 
 get '/' do
   @weekly_adoptions = Adoption.weekly_adoptions
+  @animals_needing_adoption = Animal.count_need_adoption
   erb(:index)
 end
