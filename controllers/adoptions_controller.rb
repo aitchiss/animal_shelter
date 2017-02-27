29b4301630e587e9643b8f69ad196e0901e25a39
Suzanne <suzanne.aitchison@gmail.com>
2017-02-27 15:37:07 +0000

    adoptions form now prevents mismatches, shows error message if no match

--- controllers/adoptions_controller.rb
+++ controllers/adoptions_controller.rb
@@ -23,16 +23,25 @@ end
 
 
 post '/adoptions' do
-  date = Date.today
   owner_id = params['owner_id'].to_i
+  owner = Owner.find(owner_id)
   animal_id = params['animal_id'].to_i
-  adoption = Adoption.new({
-    'owner_id' => owner_id,
-    'animal_id' => animal_id,
-    'date' => date
-    })
-  adoption.save
-  animal = Animal.find(animal_id)
-  animal.process_adoption
-  redirect to('/owners')
+
+  if owner.match(animal_id)
+
+    date = Date.today
+    adoption = Adoption.new({
+      'owner_id' => owner_id,
+      'animal_id' => animal_id,
+      'date' => date
+      })
+    adoption.save
+    animal = Animal.find(animal_id)
+    animal.process_adoption
+    redirect to('/owners')
+
+  else
+    erb(:"adoptions/error")
+
+  end
 end
