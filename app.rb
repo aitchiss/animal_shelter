1b8deca3c24c3780fe5f98a4fff81e32bc660369
Suzanne <suzanne.aitchison@gmail.com>
2017-02-27 12:07:44 +0000

    number of adoptions this week added to home page

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
 require('sinatra')
 require('sinatra/contrib/all')
 
+require_relative('./models/adoption.rb')
 require_relative('./controllers/animals_controller.rb')
 require_relative('./controllers/owners_controller.rb')
 require_relative('./controllers/adoptions_controller.rb')
 
 get '/' do
+  @weekly_adoptions = Adoption.weekly_adoptions
   erb(:index)
 end
